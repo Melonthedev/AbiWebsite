24abdbe72ee6f5f92787b9f08969e51db6dbe7dc
Marlon Jestädt <marlon2019@outlook.de>
2025-10-10 23:02:45 +0200

    Implement Code Authentication over Email

---
+++ Models/HostOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace AbiWebsite.Models;
+
+public class HostOptions
+{
+    public string? Url { get; set; }
+
+    public string? HostMail { get; set; }
+
+    public HostMailCredentials? MailCredentials { get; set; }
+
+
+    public class HostMailCredentials
+    {
+        public string? Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
