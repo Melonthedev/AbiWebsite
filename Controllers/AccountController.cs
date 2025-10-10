346961c7bd3d44caa052d1184fad78a0b529906f
Marlon Jestädt <marlon2019@outlook.de>
2025-10-11 00:37:58 +0200

    Allow Code Resending

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using AbiWebsite.Data;
+using AbiWebsite.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AbiWebsite.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : Controller {
+        private readonly AbiDbContext _db;
+        private readonly EmailService _emailService;
+
+        public AccountController(AbiDbContext db, EmailService emailService) {
+            _db = db;
+            _emailService = emailService;
+        }
+
+        [HttpPost("resendlogincode")]
+        public async Task<IActionResult> ResendLoginCode([FromForm] string name) {
+            var student = await _db.Students.FirstOrDefaultAsync(s => s.FullName == name);
+            if (student == null)
+                return Redirect("/loginwithcode?name=" + Uri.EscapeDataString(name) + "&msg=notfound");
+
+            var rnd = new Random();
+            student.LoginCode = rnd.Next(100, 1000);
+            await _db.SaveChangesAsync();
+
+            _emailService.SendLoginCodeMail(student.Email, student.FullName, student.LoginCode.ToString("D3"), isResend: true);
+            return Redirect("/loginwithcode?name=" + Uri.EscapeDataString(name) + "&msg=resent");
+        }
+    }
+}
